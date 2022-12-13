3079c8e14d9bc08e4bfff61c5db5c4129148061a
Vesperma <43173186+Vesperma@users.noreply.github.com>
2022-12-13 16:16:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,19 +21,19 @@ const USER_CONFIG = {
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    morningGreeting: false,
+    morningGreeting: true,
   
     // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    eveningGreeting: false,
+    eveningGreeting: true,
   
     // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
     weather: 0,
   
     // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    networkHot: 10,
+    networkHot: 5,
   
     // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
-    networkHotType: 'default',
+    networkHotType: 'title',
   },
   USERS: [
     {
